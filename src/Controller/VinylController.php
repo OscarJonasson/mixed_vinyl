191ebbe03643273131111e92c082e3a2d0cbc691
OscarJonasson <Oscjjonasson@gmail.com>
2023-02-15 21:20:17 +0200

    Added first template, used filter inside and a for loop

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -2,16 +2,28 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
 
-class VinylController {
+class VinylController extends AbstractController {
 
     #[Route('/')]
     public function homepage(): Response {
-        return  new Response('Title: PB n Jams');
+        $tracks = [
+            ['song' => 'track1', 'artist' => 'Me'],
+            ['song' => 'track2', 'artist' => 'Yo'],
+            ['song' => 'track3', 'artist' => 'Us'],
+            ['song' => 'track4', 'artist' => 'Them'],
+            ['song' => 'track5', 'artist' => 'Who']
+        ];
+
+        return  $this->render('vinyl/homepage.html.twig', [
+            'title' => 'PB & Jams',
+            'tracks' => $tracks
+        ]);
     }
 
     #[Route('/browse/{thisCanBeAnything}')]
